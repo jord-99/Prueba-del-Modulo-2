1ad5b4d1af3209947666b4ee8cd3b041f02bf361
Esteban Revelo <84397774+StbanJ@users.noreply.github.com>
2021-07-12 13:35:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist', 'Prueba2')))
+app.use(express.static(path.join(__dirname, 'dist', 'Prueba-del-Modulo-2')))
 
 app.all("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'Prueba2', 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist', 'Prueba-del-Modulo-2', 'index.html'))
 })
 
 app.listen(process.env.PORT || 3000)
